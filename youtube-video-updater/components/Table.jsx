8e1825d2211a8c321d083bea50f6c93437c8a2a3
Fernando <jf.palacios.sz@gmail.com>
2021-12-12 00:16:19 -0500

    fix(youtube-video-updater): add keys to table maps

--- youtube-video-updater/components/Table.jsx
+++ youtube-video-updater/components/Table.jsx
@@ -15,23 +15,23 @@ export default function Table({ columns = [], data = [] }) {
     return (
         <table {...getTableProps()} className='w-full divide-y divide-gray-200 border-collapse'>
             <thead className='bg-gray-50'>
-                {headerGroups.map(headerGroup => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map(column => (
-                    <th {...column.getHeaderProps()} className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>{column.render('Header')}</th>
+                {headerGroups.map((headerGroup, headerIndex) => (
+                <tr {...headerGroup.getHeaderGroupProps()} key={headerIndex}>
+                    {headerGroup.headers.map((column, columnIndex) => (
+                    <th {...column.getHeaderProps()} key={columnIndex} className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>{column.render('Header')}</th>
                     ))}
                 </tr>
                 ))}
             </thead>
             <tbody {...getTableBodyProps()} className='bg-white divide-y divide-gray-200'>
                 {
-                    rows.map((row, i) => {
+                    rows.map((row, rowIndex) => {
                         prepareRow(row)
                     
                         return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map(cell => {
-                                    return <td {...cell.getCellProps()} className='px-6 py-4 whitespace-nowrap'>{cell.render('Cell')}</td>
+                            <tr {...row.getRowProps()} key={rowIndex}>
+                                {row.cells.map((cell, cellIndex) => {
+                                    return <td {...cell.getCellProps()} key={cellIndex} className='px-6 py-4 whitespace-nowrap'>{cell.render('Cell')}</td>
                                 })}
                             </tr>
                         )
